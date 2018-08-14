b7cc18f854dcf93f23e78aad6520498a8a21e109
pranalighalme <ghalmepranali@gmail.com>
2018-08-14 20:16:27 +0530

    models

--- PUCSD_LiveHealth/studentMgt/models.py
+++ PUCSD_LiveHealth/studentMgt/models.py
@@ -1,6 +1,33 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+class Department(models.Model):
+	department_id = models.IntegerField(primary_key=True, editable=False)
+	name = models.CharField(max_length=30)
 
-from django.db import models
+	class Meta:
+		db_table = 'department'
 
-# Create your models here.
+class Course(models.Model):
+	course_id = models.IntegerField(primary_key=True, editable=False)
+	course_name = models.CharField(max_length=30)
+	department_id = models.ForeignKey(Department, related_name = 'course')
+ 
+	class Meta:
+		db_table = 'course'
+
+class Course_Class(models.Model):
+	class_id = models.IntegerField(primary_key=True, editable=False)
+	course_id = models.ForeignKey(Course, related_name = 'course')
+	teacher_id = models.ForeignKey(Teacher, related_name = 'course_class')
+	strength = models.IntegerField(null=False)
+
+	class Meta:
+		db_table = 'couse_class'
+
+class Subject(models.Model):
+	subject_id = models.IntegerField(primary_key=True, editable=False)
+	subject_name = models.CharField(max_length=30)
+	class_id = models.ForeignKey(Course_Class, related_name = 'subject')
+	teacher_id = models.ForeignKey(Teacher, related_name = 'subject')
+
+	class Meta:
+		db_table = 'subject'
+
