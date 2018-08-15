03dabfbf40d59bcd8b7546ff70f22e2fc854028c
vishal1026 <vishalkhandekar1026@gmail.com>
2018-08-15 09:59:51 +0530

    Merged all Model

--- PUCSD_LiveHealth/studentMgt/models.py
+++ PUCSD_LiveHealth/studentMgt/models.py
@@ -1,6 +1,112 @@
-# -*- coding: utf-8 -*-
+# * coding: utf8 *
 from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class School_user(models.Model):
+	user_id = models.IntegerField (primary_key = True)
+	user_name = models.CharField(max_length=70)
+	password = models.CharField(max_length=70)
+	is_active = models.BooleanField()
+	user_type = models.IntegerField(null=False)
+
+	class Meta:
+		db_table = 'school_user'
+
+class Parent(models.Model):	
+	parent_id = models.IntegerField(primary_key=True, editable=False)
+	user_id = models.ForeignKey(School_user, related_name = 'parent')
+	fname = models.CharField(max_length=30, null = False)
+	lname = models.CharField(max_length=30, null = False)
+	contact = models.DecimalField(max_digits=10, decimal_places=0)
+
+	class Meta:
+		db_table='parent'
+
+class Student(models.Model):
+	student_id = models.IntegerField(primary_key=True, editable=False)
+	user_id = models.ForeignKey(School_user, related_name='student')
+	fname = models.CharField(max_length=30)
+	lname = models.CharField(max_length=30)
+	contact = models.DecimalField(max_digits=10, decimal_places=0 )
+
+	class Meta:
+		db_table='student'
+
+class Teacher(models.Model):
+	teacher_id = models.IntegerField(primary_key=True, editable=False)
+	user_id = models.ForeignKey(School_user, related_name='teacher')
+	fname = models.CharField(max_length=30)
+	lname = models.CharField(max_length=30)
+	contact = models.DecimalField(max_digits=10, decimal_places=0)
+
+	class Meta:
+		db_table = 'teacher'
+
+class Department(models.Model):
+	department_id = models.IntegerField(primary_key=True, editable=False)
+	name = models.CharField(max_length=30)
+
+	class Meta:
+		db_table = 'department'
+
+class Course(models.Model):
+	course_id = models.IntegerField(primary_key=True, editable=False)
+	course_name = models.CharField(max_length=30)
+	department_id = models.ForeignKey(Department, related_name = 'course')
+ 
+	class Meta:
+		db_table = 'course'
+
+class Course_Class(models.Model):
+	class_id = models.IntegerField(primary_key=True, editable=False)
+	course_id = models.ForeignKey(Course, related_name = 'course')
+	teacher_id = models.ForeignKey(Teacher, related_name = 'course_class')
+	strength = models.IntegerField(null=False)
+
+	class Meta:
+		db_table = 'couse_class'
+
+class Subject(models.Model):
+	subject_id = models.IntegerField(primary_key=True, editable=False)
+	subject_name = models.CharField(max_length=30)
+	class_id = models.ForeignKey(Course_Class, related_name = 'subject')
+	teacher_id = models.ForeignKey(Teacher, related_name = 'subject')
+
+	class Meta:
+		db_table = 'subject'
+
+class Student_in_class(models.Model):
+	student_class_id = models.IntegerField(primary_key=True, editable=False)
+	roll_no = models.IntegerField(null=False)
+	class_id = models.ForeignKey(Course_Class, related_name = 'student_in_class')
+	student_id = models.ForeignKey(Student, related_name = 'student_in_class')
+	
+	class Meta:
+		db_table = 'student_in_class'
+
+class Exam(models.Model):
+	exam_id = models.IntegerField(primary_key=True, editable=False)
+	date = models.DateField()
+	subject_id = models.ForeignKey(Subject, related_name = 'exam')
+
+	class Meta:
+		db_table = 'exam'
+		
+class Marks(models.Model):
+	exam_id = models.ForeignKey(Exam, related_name = 'marks')
+	student_class_id = models.ForeignKey(Student_in_class, related_name = 'marks')
+	marks_obtained = models.IntegerField(null=False)
+	marks_out_of = models.IntegerField(null=False)
+
+	class Meta:
+		db_table = 'marks'
+
+class Attendance(models.Model):
+	attendance_id = models.IntegerField(primary_key=True, editable=False)
+	date = models.DateField()
+	class_id = models.ForeignKey(Course_Class, related_name = 'attendance')
+	attendance_data = models.TextField( null = False )
+
+	class Meta:
+		db_table = 'attendance'
